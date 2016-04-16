4dc317c64740e38bc781b82fb6db922e0fe51a81
Abualsamid <ahmad@abualsamid.com>
2016-04-15 21:23:37 -0400

    config changes.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 var webpackHotMiddleware = require('webpack-hot-middleware')
@@ -5,10 +6,24 @@ var config = require('./webpack.config')
 var express = require('express')
 
 var app = new (express)()
-var port = 3001
+
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3001;
 
 var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackDevMiddleware(compiler,
+  { // noInfo: true,
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+
+ }))
 app.use(webpackHotMiddleware(compiler))
 
 app.use(express.static(__dirname + '/public'));
